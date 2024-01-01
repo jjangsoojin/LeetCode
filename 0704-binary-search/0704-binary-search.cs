1d4bf722ec4b143b3d50b7c16f2f8671f9cb196c
Soojin Ahn <121205497+jjangsoojin@users.noreply.github.com>
2024-01-01 19:25:58 +0900

    Time: 240 ms (5.03%), Space: 47.8 MB (97.96%) - LeetHub

--- 0704-binary-search/0704-binary-search.cs
+++ 0704-binary-search/0704-binary-search.cs
@@ -1,15 +1,25 @@
 public class Solution {
     public int Search(int[] nums, int target) {
         
-      for (int i = 0; i < nums.Length; i++)
-            {
-                if (nums[i] == target)
-                {
-                    return i;
-                }
+   int left = 0;
+            int right = nums.Length - 1;
 
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;  // start를 더하지 않으면 배열의 시작 인덱스를 고려하지 않은 중간값이 됨
+
+                if(nums[mid] == target)
+                {
+                    return mid;
+                }else if(nums[mid] < target)
+                {
+                    left = mid + 1;
+                }else
+                {
+                    right = mid - 1;
+                }
             }
-       return -1;
-        
+
+            return -1; // Target not found in the array
     }
 }

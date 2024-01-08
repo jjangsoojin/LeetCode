b51fe72574467f186d17605617221e72b6a963f8
Soojin Ahn <121205497+jjangsoojin@users.noreply.github.com>
2024-01-08 14:25:44 +0900

    Time: 49 ms (99.18%), Space: 40.9 MB (17.91%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cs
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cs
@@ -11,22 +11,23 @@
  */
 public class Solution {
     public ListNode ReverseList(ListNode head) {
-          if (head == null || head.next == null)
-  {
-      return head; // 빈 리스트이거나 단일 노드인 경우 그대로 반환
-  }
+           if (head == null || head.next == null)
+         {
+             return head; // 빈 리스트이거나 단일 노드인 경우 그대로 반환
+         }
 
-  ListNode prev = null;
-  ListNode current = head;
+         ListNode prev = null;
+         ListNode current = head;
 
-  while (current != null)
-  {
-      ListNode nextNode = current.next;
-      current.next = prev;
-      prev = current;
-      current = nextNode;
-  }
+         while (current != null)
+         {
+             ListNode nextNode = current.next;
+             current.next = prev;
+             prev = current;
+             current = nextNode;
 
-  return prev; // 역순으로 뒤집은 리스트의 새로운 헤드를 반환
+         }
+
+         return prev; // 역순으로 뒤집은 리스트의 새로운 헤드를 반환
     }
 }

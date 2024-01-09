409abe77fa931a30a7528889c1eb0da40197b165
Soojin Ahn <121205497+jjangsoojin@users.noreply.github.com>
2024-01-09 17:47:38 +0900

    Time: 105 ms (37.55%), Space: 46 MB (23.92%) - LeetHub

--- 0169-majority-element/0169-majority-element.cs
+++ 0169-majority-element/0169-majority-element.cs
@@ -1,7 +1,7 @@
 public class Solution {
     public int MajorityElement(int[] nums) {
-        
-       
+        /*
+        // 기존에 짰던 코드 
          Dictionary<int, int> dic = new Dictionary<int, int>();
          int maxvalue = 0;
          int max = 0;
@@ -34,6 +34,25 @@ public class Solution {
          }
 
          return max;
+*/
+     // O(n) 시간복잡도
+        
+     int candidate = 0;
+        int count = 0;
 
+        foreach (int num in nums) {
+            if (count == 0) {       
+                candidate = num;            // 만약 count가 0이라면 새로운 후보를 선택하고 count를 1로 설정
+                count = 1;
+            } else if (candidate == num) {      // 현재 요소가 후보와 같다면 count를 증가
+                count++;    
+            } else {                            // 현재 요소가 후보와 다르다면 count를 감소
+                count--;
+            }
+        }
+
+        // Majority element 항상 존재한다고 가정하였으므로 따로 체크할 필요 없음
+        return candidate;
+        
     }
 }

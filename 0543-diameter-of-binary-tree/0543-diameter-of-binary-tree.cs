c394a9d7774c224d23db61975c8783bc6f31ea6d
Soojin Ahn <121205497+jjangsoojin@users.noreply.github.com>
2024-01-10 15:03:22 +0900

    Time: 70 ms (87.89%), Space: 42.7 MB (6.76%) - LeetHub

---
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cs
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    private int diameter;
+    
+    public int DiameterOfBinaryTree(TreeNode root) {
+            
+            diameter = 0;
+            Depth(root);
+            return diameter;
+        }
+
+        public int Depth(TreeNode node)
+        {
+            if (node == null)
+                return 0;
+
+
+            int left = Depth(node.left);
+            int right = Depth(node.right);
+
+            diameter = Math.Max(diameter, left + right);
+
+
+            return Math.Max(left,right) + 1;
+        }
+        
+}
